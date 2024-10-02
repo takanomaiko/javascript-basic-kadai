477c70a751119e9982231607df6ade1de75236a9
takanomaiko <takanomaiko0816@gmail.com>
2024-10-02 13:16:08 +0900

    Delete kadai_021 directory

--- kadai_021/event.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('btn');
-const text = document.getElementById('text');
-
-btn.addEventListener('click', ()=>{
-    setTimeout(() => {
-        const text = document.getElementById("text").textContent = 'ボタンをクリックしました';
-    },2000);
-});
